ddf24a4b1775b6b7c0d2614460177a283fd6a73c
king10tech <hsw0194@gmail.com>
2017-08-10 12:20:06 +0900

    comment modified

--- MP_TFIDF_NOUNCHUNK_CALC_ERROR.py
+++ MP_TFIDF_NOUNCHUNK_CALC_ERROR.py
@@ -2,7 +2,7 @@ from __future__ import division
 from __future__ import print_function
 
 import csv
-import multiprocessing
+from multiprocessing import Process, Array
 import sys
 
 reload(sys)
@@ -485,128 +485,130 @@ def DFCHECK(setStart, setEnd):
             setStart) + ' to ' + str(setEnd) + '.csv', 'w+')
     writer_OUTPUT = csv.writer(ValidOutput, delimiter=',', quotechar=',')
 
-    TargetWordIdx = 0
-    errorCount=0
+    targetWordIdx = 0
+    errorCountInRange = 0
 
     for wordAndDFCounts in reader:
-        TargetWordIdx += 1
+        targetWordIdx += 1
 
-        if TargetWordIdx < setStart: # Pointer of DFCHECK not reached startpoint yet.
+        if targetWordIdx < setStart:  # Pointer of DFCHECK not reached startpoint yet.
             continue
 
-        if TargetWordIdx > setEnd: # Works done in range of setstart~setend.
-            return errorCount
+        if targetWordIdx > setEnd:  # Works done in range of setstart~setend.
+            return errorCountInRange
 
-        word_DFCOUNTs = []
-        word_DFCOUNTs = wordAndDFCounts
+        word_DFCOUNTsStore = []
+        word_DFCOUNTsStore = wordAndDFCounts
+        word_DFCOUNTsStore[0] = word_DFCOUNTsStore[0].lstrip(
+            ' ')  # word_DFCOUNTsStore is word that needed to be checked.
+
         DFCountStore = []
         outputText = []
-        firstDF = 0  # firstDF is compared with word_DFCOUNTs[5],which is old DFCOUNT of 1980 in inputfile.
+        firstDF = 0  # firstDF is compared with word_DFCOUNTsStore[5],which is old DFCOUNT of 1980 in inputfile.
         secondDF = 0  # secondDF is similar to firstDF.
         thirdDF = 0  # thirdDF is similar to firstDF.
         isError = False  # if isError boolean value is true,then,word in row have invalid DFCOUNT value.
 
-        word_DFCOUNTs[0] = word_DFCOUNTs[0].lstrip(' ')
-        print("checking for words : " + word_DFCOUNTs[0])
+        print("checking for words : " + word_DFCOUNTsStore[0])
 
         csv_1980 = open('1980.csv', 'rU')
         reader1980 = csv.reader(csv_1980, delimiter=',', quotechar=',')
         for patent_1980 in reader1980:
             for patword_1980 in patent_1980:
                 patword_1980 = patword_1980.lower().lstrip(' ')
-                if str(patword_1980) == str(word_DFCOUNTs[0]):
+                if str(patword_1980) == str(word_DFCOUNTsStore[0]):
                     firstDF = firstDF + 1
                     break
         csv_1980.close()
 
-        print("compared with old : " + word_DFCOUNTs[5] + " , " + str(firstDF))
+        print("compared with old : " + word_DFCOUNTsStore[5] + " , " + str(firstDF))
 
-        if firstDF != int(word_DFCOUNTs[5]):
+        if firstDF != int(word_DFCOUNTsStore[5]):
             # check old DFCOUNT. If it is invalid, DFCALC procedure will be called.
-            print("Error at " + word_DFCOUNTs[0] + " in 1980")  # Tell user to old DFCOUNT is invalid.
+            print("Error at " + word_DFCOUNTsStore[0] + " in 1980")  # Tell user to old DFCOUNT is invalid.
             isError = True  # Set error occur value to true.
-            DFCountStore = DFCALC(word_DFCOUNTs)  # So to correct invalid DFCOUNT, call DFCALC.
+            DFCountStore = DFCALC(word_DFCOUNTsStore)  # So to correct invalid DFCOUNT, call DFCALC.
 
-        elif firstDF == int(word_DFCOUNTs[5]):
-            print("No error at " + word_DFCOUNTs[0] + " in 1980")
+        elif firstDF == int(word_DFCOUNTsStore[5]):
+            print("No error at " + word_DFCOUNTsStore[0] + " in 1980")
 
         csv_2000 = open('2000.csv', 'rU')
         reader2000 = csv.reader(csv_2000, delimiter=',', quotechar=',')
         for patent_2000 in reader2000:
             for patword_2000 in patent_2000:
                 patword_2000 = patword_2000.lower().lstrip(' ')
-                if str(patword_2000) == str(word_DFCOUNTs[0]):
+                if str(patword_2000) == str(word_DFCOUNTsStore[0]):
                     secondDF = secondDF + 1
                     break
         csv_2000.close()
 
-        print("compared with old : " + word_DFCOUNTs[25] + " , " + str(secondDF))
+        print("compared with old : " + word_DFCOUNTsStore[25] + " , " + str(secondDF))
 
-        if secondDF != int(word_DFCOUNTs[25]) and isError is False:
+        if secondDF != int(word_DFCOUNTsStore[25]) and isError is False:
             # check old DFCOUNT. If it is invalid, DFCALC procedure will be called.
             # And if isError is True, it means that DFCALC was already called, so don't need to do that again.
-            print("Error at " + word_DFCOUNTs[0] + " in 2000")  # Tell user to old DFCOUNT is invalid.
+            print("Error at " + word_DFCOUNTsStore[0] + " in 2000")  # Tell user to old DFCOUNT is invalid.
             isError = True  # Set error occur value to true.
-            DFCountStore = DFCALC(word_DFCOUNTs)  # So to correct invalid DFCOUNT, call DFCALC.
+            DFCountStore = DFCALC(word_DFCOUNTsStore)  # So to correct invalid DFCOUNT, call DFCALC.
 
-        elif secondDF != int(word_DFCOUNTs[25]) and isError is True:
-            print("Error at " + word_DFCOUNTs[0] + " in 2000 but correction already done.")
+        elif secondDF != int(word_DFCOUNTsStore[25]) and isError is True:
+            print("Error at " + word_DFCOUNTsStore[0] + " in 2000 but correction already done.")
 
-        elif secondDF == int(word_DFCOUNTs[25]):
-            print("No error at " + word_DFCOUNTs[0] + " in 2000")
+        elif secondDF == int(word_DFCOUNTsStore[25]):
+            print("No error at " + word_DFCOUNTsStore[0] + " in 2000")
 
         csv_2011 = open('2011.csv', 'rU')
         reader2011 = csv.reader(csv_2011, delimiter=',', quotechar=',')
         for patent_2011 in reader2011:
             for patword_2011 in patent_2011:
                 patword_2011 = patword_2011.lower().lstrip(' ')
-                if str(patword_2011) == str(word_DFCOUNTs[0]):
+                if str(patword_2011) == str(word_DFCOUNTsStore[0]):
                     thirdDF = thirdDF + 1
                     break
         csv_2011.close()
 
-        print("compared with old : " + word_DFCOUNTs[36] + " , " + str(thirdDF))
+        print("compared with old : " + word_DFCOUNTsStore[36] + " , " + str(thirdDF))
 
-        if thirdDF != int(word_DFCOUNTs[36]) and isError is False:
+        if thirdDF != int(word_DFCOUNTsStore[36]) and isError is False:
             # check old DFCOUNT. If it is invalid, DFCALC procedure will be called.
             # And if isError is True, it means that DFCALC was already called, so don't need to do that again.
-            print("Error at " + word_DFCOUNTs[0] + " in 2010")  # Tell user to old DFCOUNT is invalid.
+            print("Error at " + word_DFCOUNTsStore[0] + " in 2010")  # Tell user to old DFCOUNT is invalid.
             isError = True  # Set error occur value to true.
-            DFCountStore = DFCALC(word_DFCOUNTs)  # So to correct invalid DFCOUNT, call DFCALC.
+            DFCountStore = DFCALC(word_DFCOUNTsStore)  # So to correct invalid DFCOUNT, call DFCALC.
 
-        elif thirdDF != int(word_DFCOUNTs[36]) and isError is True:
-            print("Error at " + word_DFCOUNTs[0] + " in 2010 but correction already done.")
+        elif thirdDF != int(word_DFCOUNTsStore[36]) and isError is True:
+            print("Error at " + word_DFCOUNTsStore[0] + " in 2010 but correction already done.")
 
-        elif thirdDF == int(word_DFCOUNTs[36]):
-            print("No error at " + word_DFCOUNTs[0] + " in 2010")
+        elif thirdDF == int(word_DFCOUNTsStore[36]):
+            print("No error at " + word_DFCOUNTsStore[0] + " in 2010")
 
         if isError is True:
             # if isError value is true ,it means that fucn DFCALC was called.
             # so old and invalid DFCOUNT in word_DFCOUNT sholud be changed.
             for i in range(1, 42):
-                word_DFCOUNTs[i] = DFCountStore[i - 1]
-            errorCount+=1
+                word_DFCOUNTsStore[i] = DFCountStore[i - 1]
+            errorCountInRange += 1
 
-        DFCOUNTtext = str(word_DFCOUNTs[1:])
-        output = ",".join([str(word_DFCOUNTs[0]), str(DFCOUNTtext)])
+        DFCOUNTtext = str(word_DFCOUNTsStore[1:])
+        output = ",".join([str(word_DFCOUNTsStore[0]), str(DFCOUNTtext)])
         outputText.append(output)
 
         outputText = str(outputText)
         writer_OUTPUT.writerow(outputText.split(","))
 
-        print("Invalid DFCOUNT correction of " + str(TargetWordIdx) + " is done,\n\n")
+        print("Invalid DFCOUNT correction of " + str(targetWordIdx) + " is done,\n\n")
 
     ValidOutput.close()
     ERRORINPUT.close()
 
 
 def DFCALC(word_DFCOUNTs):
-    DFCOUNT = multiprocessing.Array('i', 41)
+    DFCOUNT = Array('i', 41)
 
-    p1 = multiprocessing.Process(target=DFCALCforward, args=(word_DFCOUNTs[0], DFCOUNT))
-    p2 = multiprocessing.Process(target=DFCALCbackward, args=(word_DFCOUNTs[0], DFCOUNT))
-    p3 = multiprocessing.Process(target=DFCALC3, args=(word_DFCOUNTs[0], DFCOUNT))
-    p4 = multiprocessing.Process(target=DFCALC4, args=(word_DFCOUNTs[0], DFCOUNT))
+    p1 = Process(target=DFCALCforward, args=(word_DFCOUNTs[0], DFCOUNT))
+    p2 = Process(target=DFCALCbackward, args=(word_DFCOUNTs[0], DFCOUNT))
+    p3 = Process(target=DFCALC3, args=(word_DFCOUNTs[0], DFCOUNT))
+    p4 = Process(target=DFCALC4, args=(word_DFCOUNTs[0], DFCOUNT))
     p1.start()
     p2.start()
     p3.start()
@@ -620,4 +622,4 @@ def DFCALC(word_DFCOUNTs):
     return DFCOUNTRETURN
 
 
-totalErrorCount=DFCHECK(18022, 18023)
+totalErrorCount = DFCHECK(18022, 18023)  # First parameter is starting word and second one is end word.
